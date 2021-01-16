962b99c85341397594c9ecd169ff987f4ca3e95e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-16 21:51:24 +0530

    Created request for api. Worked on get all the movies.

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const APIKEY = "e41f742e4095524e33c3fb7b9c8cb90a";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
