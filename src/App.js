31106caa8ec05f8e5c650c7052699970e19b7b39
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 21:09:16 +0530

    passed another prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Row from './Row'
+import { requests } from "./requests";
 
 function App() {
   return (
 
     <div className="App">
-      <h1> hi</h1>   
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <h1> Netflix Clone Project </h1>   
+      <Row title="NETFLIX ORIGINALS"  fetchUrl={requests. fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />      
     </div>
     
   );
