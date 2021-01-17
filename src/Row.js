3df175e819acf6637135327d4491fd65c56d2958
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 20:07:19 +0530

    created state for movies

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Row({ title }) {
+  const [movies, setMovies] = useState([]);
+
   return (
+    
     <div>
 
       {/* title */}
