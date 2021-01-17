3224d14006cad9f3519f414c0dfbbf03424c9d92
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 21:47:56 +0530

    working on get all the movies and build rows

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from './axios'
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
+  // A snippet of code which runs based on a specific condition
+
+  useEffect(() => {
+     // if [], which means => run once when the row loads, and don't run it again.
+     async function fetchData() {
+       const request = await axios.get(fetchUrl); //"https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213", 
+       console.log(request);
+       return request;       
+     }
+     fetchData();
+  }, []);
+
   return (
-    
+
     <div>
 
       {/* title */}
