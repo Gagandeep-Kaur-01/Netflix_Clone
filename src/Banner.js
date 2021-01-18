6278bbee25b80f5e068e021fa04b78a722058588
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-18 21:50:24 +0530

    to get the single movie poster from the array randomly

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,25 @@
 import React, {useEffect, useState} from 'react'
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            //
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results
+                [
+                    Math.floor(Math.random() * request.data.results.length - 1) 
+                    //to get random object/ movie in the array or to get the object from the array randomly.
+                ]
+            );
+            return request;          
         }
-        fetchData()
+        fetchData();
     }, []);
+    
     return (
         <header> {/* <<<Background image */}
             {/* title */}
