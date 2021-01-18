11305b89e7b5131460555aa1f73f2dd0f600ce6a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-18 15:56:18 +0530

    added & displayed more movies categories

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
     <div className="App">
       <h1> Netflix Clone Project </h1>   
       <Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />      
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />  
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />    
+      <Row title="Action Movies" fetchUrl={requests. fetchActionMovies} />    
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />    
+      <Row title="Horror Movies" fetchUrl={requests. fetchHorrorMovies} />    
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />       
+
     </div>
     
   );
