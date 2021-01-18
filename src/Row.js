be3d4a69ba62e77dd801cd1e4ddf21c06f1bc01a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-18 17:25:12 +0530

    added condition for the style, to get the larger row

--- src/Row.js
+++ src/Row.js
@@ -46,7 +46,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map(movie => (          
           <img                         //"/obLBdhLxheKg8Li1qO11r2SwmYO.jpg"
             key={movie.id}
-            className="row_poster"
+            className={`row_poster ${isLargeRow && "row_posterLarger"}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name}/>        /* if it doesn't get the image, it'll just get the name */
         ))}
