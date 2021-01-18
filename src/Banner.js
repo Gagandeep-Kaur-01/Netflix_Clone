64c263ac7af74794fef3b8ce013b5bfb2c04448a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-18 21:35:35 +0530

    created state, because on every refreshing the banner has to be changed

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
     return (
         <header> {/* <<<Background image */}
             {/* title */}
