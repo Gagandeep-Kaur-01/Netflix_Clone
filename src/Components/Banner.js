0f0d830ce6125285b21d8fea46bb45713dc797bc
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 16:08:27 +0530

    created Components folder and put all the components to this folder

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import axios from './axios';
-import requests from './Requests';
+import axios from '../axios';
+import requests from '../Requests';
 import './Banner.css'
 
 function Banner() {
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from './axios';
+import axios from '../axios';
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/"
