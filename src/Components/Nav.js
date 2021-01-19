075789f9e24cca7c60dafee7d4d6606c0f65acf2
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 17:18:32 +0530

    added condition for nav style

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -17,7 +17,7 @@ function Nav() {
   }, []);
 
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav_black"}`}>
             <img
                className="nav_logo"
                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
