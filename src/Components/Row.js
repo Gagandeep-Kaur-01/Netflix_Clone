a2f93b7583802a4d6bc547ce900c1eae2a0d35b9
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 17:57:32 +0530

    added auplay option

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -26,6 +26,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     width: "100%",
     playerVars: {
       //https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
     },
   };
   
