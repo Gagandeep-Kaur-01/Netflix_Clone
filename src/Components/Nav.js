e1411f1ddbe079355eabb60c990cb2ff9fab6a98
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 17:15:55 +0530

    added function for handling the scroll

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Nav.css"
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
     return (
         <div className="nav">
             <img
@@ -14,8 +28,7 @@ function Nav() {
                className="nav_avatar"
                src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
                alt="Netflix Logo"
-            />              
-
+            /> 
         </div>
     )
 }
