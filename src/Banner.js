e3a1fc90f552727b129a7133360fc62386fbd2be
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 12:19:47 +0530

    imported css file

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import axios from './axios';
 import requests from './Requests';
+import './Banner.css'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
