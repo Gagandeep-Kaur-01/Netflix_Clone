7c67ed438c30a5edf58b84282f42e71216ea8ab0
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 16:02:01 +0530

    added netflix logo

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Nav() {
     return (
-        <div>
+        <div className="nav">
+            <img
+               className="nav_logo"
+               src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+               alt="Netflix Logo"
+            />              
 
         </div>
     )
