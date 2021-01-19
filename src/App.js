687245e2b583e816e8c6ec98a7ee69b643d66534
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-19 15:41:25 +0530

    working on build navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Row from './Row'
 import requests from "./Requests";
-import Banner from "./Banner"
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
 
     <div className="app"> 
-      {/* Nav */}
-      {/* Banner */}
+      <Nav />
       <Banner />
       <Row 
         title="NETFLIX ORIGINALS"  
